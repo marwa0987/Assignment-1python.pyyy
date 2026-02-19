a595c5b24fbc9cff60481f6a723beb95c73aa56c
marwa0987 <marwafaqir150@gmail.com>
2026-02-19 09:09:21 -0800

    Add files via upload

---
+++ week 2 assigment 2.py
@@ -0,0 +1,88 @@
+# partA:
+# Loop: 
+# Question 1:
+# Print numbers from 1 to 10 using a for loop?
+# Answer :
+# code:
+for i in range (1 ,11):
+print (i)
+# Question 2:
+# Print even numbers from 2 to 20 using a for loop ?
+# Answer :
+# code :
+for i in range(2, 21, 2):
+    print(i)
+# Question 3 :
+# Print all character of the string "python" using a loop ? 
+# Answer :
+# code :
+for ch in "Python":
+    print (ch)
+# Question 4:
+# Print numbers from 5 to 1 using a loop ?
+# Answer :
+# code :
+for i in range(5 , 21 , -1):
+    print (i)
+# Question 5:
+# Use a while loop to print numbers from 1 to 5 ?
+# Answer :
+# Code :
+i =1
+while i<=5:
+    print(i)
+    i+=1
+# PART B
+# FUNCTION :
+# Question 6:
+# Create a function that prints "Hello Python" when called ?
+# Answer :
+# Code :
+def greet():
+    print("Hello Python")
+greet()
+# Question 7:
+# Create a funtion that takes one number and prints it ?
+# Answer:
+# Code :
+def print_number(num):
+    print(num)
+print _number(7)
+# Question 8:
+# Create a function that takes two numbers and prints their sum ?
+# Answer:
+# Code :
+def add_numbers(a, b):
+    print(a + b)
+add_numbers(4, 7)
+# Question 9:
+# Create a function that takes one number and  checks whether it is even or odd ?
+# Answer:
+# Code:
+def check_even_odd(num):
+    if num % 2 == 0:
+        print("Even")
+    else:
+        Print("odd")
+check_even_odd(7)
+# Question 10 :
+# Create a function that prints numbers from 1 to 5 using a loop ?
+# Answer:
+# Coding :
+def print_numbers()
+for i in range(1 ,6):
+    print(i)
+printi_number()
+# MINI PROJRCT:NUMBER TABLE GENERATOR"
+# FINCTION TO PRINT (1 TO 10)
+def number_table(num):
+    for i in ramge(1 ,11):
+        print(num, "x" , i ,"=", num*)
+i)
+number_table(3)        
+    
+
+
+
+
+            
